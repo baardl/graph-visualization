ca665d512ad04cad0850e413fce5cb20869416a0
bard lind <bard.lind@gmail.com>
2014-01-02 22:05:54 +0100

    Added test. That fails :-)

---
+++ dynamic/src/test/java/com/bardlind/graph/NodeWithLinksTest.java
@@ -0,0 +1,34 @@
+package com.bardlind.graph;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertNotNull;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: baardl
+ * Date: 1/2/14
+ * Time: 10:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NodeWithLinksTest {
+    @Test
+    public void testGetAdjacencies() throws Exception {
+        ArrayList<NodeWithLinks> graph = new ArrayList<>();
+        NodeWithLinks node1 = new NodeWithLinks("node1", "node1");
+        node1.addAdjacency(new Link("node1", "node2"));
+        node1.addAdjacency(new Link("node1", "node3"));
+        graph.add(node1);
+
+
+        assertNotNull(graph);
+        assertNotNull(node1);
+        assertNotNull(node1.getAdjacencies());
+        Link[] adjecencies = node1.getAdjacencies();
+
+
+
+    }
+}
