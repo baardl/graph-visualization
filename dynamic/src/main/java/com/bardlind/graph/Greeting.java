5346f38f6435840848040b4d1d11dd97f3645919
bard lind <bard.lind@gmail.com>
2014-01-02 20:11:45 +0100

    Standalone java -jar dynamic.jar

---
+++ dynamic/src/main/java/com/bardlind/graph/Greeting.java
@@ -0,0 +1,11 @@
+package com.bardlind.graph;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: baardl
+ * Date: 1/2/14
+ * Time: 6:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Greeting {
+}
